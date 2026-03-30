75234da2fff1bb3f2c52f78cb01b140c77c54dcc
pupp3ronii <pakkapong55059@gmail.com>
2026-03-30 08:51:50 +0000

    update main and monster

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
- 
+    monster m1("Namo",20,1);
+    monster m2("Tawan",20,1);
+    monster x[5]({"PP",5},{"Puppy",10});
     return 0;
 }
